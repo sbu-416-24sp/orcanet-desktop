0d9bf1cd60bced5efd979559f8785d4f1c9be53f
Adam Lipson <40801946+Adamlip1334@users.noreply.github.com>
2024-04-15 18:25:34 -0400

    copied over peers code

---
+++ src/renderer/src/peers/fakeSeeds.tsx
@@ -0,0 +1,62 @@
+import { Seed } from "./columns";
+
+const fakeSeeds: Seed[] = [
+  {
+    location: "USA, Clifton",
+    latency: "11ms",
+    peerID:  "1203 fUr4",
+    connection: "ip4.tcp",
+    openStreams: "/ipfs/bitswap/1.2.0/, /ipfs/cad/1.0.0", 
+    flagUrl:"https://www.worldometers.info//img/flags/small/tn_us-flag.gif"
+  },
+  {
+    location: "ARG, Buenos Aires",
+    latency: "34ms",
+    peerID:  "762 zDl9",
+    connection: "ip4.tcp",
+    openStreams: "/ipfs/bitswap/1.2.0/, /ipfs/cad/1.0.0", 
+    flagUrl:"https://www.worldometers.info//img/flags/small/tn_ar-flag.gif"
+  },
+  {
+    location: "UK, Bolton",
+    latency: "11ms",
+    peerID:  "1203 fUr4",
+    connection: "ip4.tcp",
+    openStreams: "/ipfs/bitswap/1.2.0/, /ipfs/cad/1.0.0", 
+    flagUrl:"https://www.worldometers.info//img/flags/small/tn_uk-flag.gif"
+  },
+  {
+    location: "USA, Texas",
+    latency: "51ms",
+    peerID:  "1203 fUr4",
+    connection: "ip4.tcp",
+    openStreams: "/ipfs/bitswap/1.2.0/, /ipfs/cad/1.0.0", 
+    flagUrl:"https://www.worldometers.info//img/flags/small/tn_us-flag.gif"
+  },
+  {
+    location: "USA, California",
+    latency: "11ms",
+    peerID:  "1203 fUr4",
+    connection: "ip4.tcp",
+    openStreams: "/ipfs/bitswap/1.2.0/, /ipfs/cad/1.0.0", 
+    flagUrl:"https://www.worldometers.info//img/flags/small/tn_us-flag.gif"
+  },
+  {
+    location: "USA, Alabama",
+    latency: "11ms",
+    peerID:  "1203 fUr4",
+    connection: "ip4.tcp",
+    openStreams: "/ipfs/bitswap/1.2.0/, /ipfs/cad/1.0.0", 
+    flagUrl:"https://www.worldometers.info//img/flags/small/tn_us-flag.gif"
+  },
+  {
+    location: "Uruguay, Montevideo",
+    latency: "11ms",
+    peerID:  "1203 fUr4",
+    connection: "ip4.tcp",
+    openStreams: "/ipfs/bitswap/1.2.0/, /ipfs/cad/1.0.0", 
+    flagUrl:"https://www.worldometers.info//img/flags/small/tn_uy-flag.gif"
+  },
+];
+
+export default fakeSeeds;
